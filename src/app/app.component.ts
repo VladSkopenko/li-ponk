74abf1d3b4b422b664e81abdae2a7c4229b0b6a0
VladSkopenko <skopirka2k17@gmail.com>
2024-10-11 12:11:15 +0300

    first import and first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component'
 
-@Component({
+@Component({                  //Декоратор тому що так потрібно
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ProfileCardComponent],  // Тут залежності
+
+
+
+  templateUrl: './app.component.html',  // Посилання на хтмл
+  styleUrl: './app.component.scss'     // Посилання на стиль
 })
 export class AppComponent {
   title = 'li-ponk';
