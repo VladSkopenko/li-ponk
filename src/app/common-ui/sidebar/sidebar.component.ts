fc7e1e30c7b859c078f6f830265eca3549cb000b
VladSkopenko <skopirka2k17@gmail.com>
2024-10-21 09:06:41 +0300

    svg complete :

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
+
 export class SidebarComponent {
 
 }
--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -3,16 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'svg[icon]',
   standalone: true,
-  imports: [],
   template: '<svg:use [attr.href]="href"></svg:use>',
-  styles: ['']
+  styles: [''],
 })
+
+
 export class SvgIconComponent {
-
-  @Input() icon: string = ''
-
+  @Input() icon: string = '';
 
   get href(): string {
-     return `/assets/svg/${this.icon}.svg#${this.icon}-icon`;
+    return `/assets/svg/${this.icon}.svg#${this.icon}`;
   }
 }
