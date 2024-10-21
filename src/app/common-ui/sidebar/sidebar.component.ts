71e0d403d2510355218fcd2726289f11b0d63036
VladSkopenko <skopirka2k17@gmail.com>
2024-10-21 09:48:33 +0300

    comple svg!!!!!!!!!!!!!!!!!!!

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgModule} from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [CommonModule, SvgIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 
 export class SidebarComponent {
-
+ menuItems: { label: string; icon: string; link: string }[] = [
+   {
+     label: "My profile",
+     icon: 'home',
+     link: ''
+   },
+   {
+     label: "Chats",
+     icon: 'chats',
+     link: 'chats'
+   },
+   {
+     label: "Search",
+     icon: 'search',
+     link: 'search'
+   },
+ ]
 }
